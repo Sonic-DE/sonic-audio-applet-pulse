79c0e952a5044653f632173279aaaa495b4f3075
Bharadwaj Raju <bharadwaj.raju777@protonmail.com>
2022-11-02 13:24:45 +0530

    Add SPDX license statements

--- src/qml/globalconfigskeleton.h
+++ src/qml/globalconfigskeleton.h
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2022 Bharadwaj Raju <bharadwaj.raju777@protonmail.com>
+
+    SPDX-License-Identifier: GPL-3.0-or-later
+*/
+
 #include <KConfigSkeleton>
 #include <KConfigWatcher>
 
