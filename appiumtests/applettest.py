d60342ddcdee09af53dbf05983945266b291a57d
Fushan Wen <qydwhotmail@gmail.com>
2023-11-05 22:21:13 +0800

    applettest: test device list

--- appiumtests/applettest.py
+++ appiumtests/applettest.py
@@ -4,6 +4,8 @@
 # SPDX-FileCopyrightText: 2023 Fushan Wen <qydwhotmail@gmail.com>
 # SPDX-License-Identifier: GPL-2.0-or-later
 
+import subprocess
+import sys
 import unittest
 from typing import Final
 
@@ -11,7 +13,10 @@ import gi
 from appium import webdriver
 from appium.options.common.base import AppiumOptions
 from appium.webdriver.common.appiumby import AppiumBy
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
+from appium.webdriver.webdriver import ExtensionBase
+from appium.webdriver.webelement import WebElement
 
 gi.require_version('Gdk', '3.0')
 from gi.repository import Gdk
@@ -39,33 +44,55 @@ def keyval_to_keycode(key_val: int) -> int:
     return keys[0].keycode
 
 
-class DesktopTest(unittest.TestCase):
+class SetValueCommand(ExtensionBase):
+
+    def method_name(self):
+        return "set_value"
+
+    def set_value(self, element: WebElement, value: str):
+        """
+        Set the value on this element in the application
+        Args:
+            value: The value to be set
+        """
+        data = {
+            "id": element.id,
+            "text": value,
+        }
+        return self.execute(data)["value"]
+
+    def add_command(self):
+        return "post", "/session/$sessionId/appium/element/$id/value"
+
+
+class AppletTest(unittest.TestCase):
     """
-    Tests for the desktop package
+    Tests for the widget
     """
 
     driver: webdriver.Remote
+    options = AppiumOptions()
+    pulseaudio: subprocess.Popen | None = None
 
     @classmethod
     def setUpClass(cls) -> None:
         """
         Initializes the webdriver
         """
-        options = AppiumOptions()
-        options.set_capability("app", f"plasmawindowed -p org.kde.plasma.nano {WIDGET_ID}")
-        options.set_capability("environ", {
+        cls.options.set_capability("app", f"plasmawindowed -p org.kde.plasma.nano {WIDGET_ID}")
+        cls.options.set_capability("environ", {
             "QT_FATAL_WARNINGS": "1",
             "QT_LOGGING_RULES": "qt.accessibility.atspi.warning=false;kf.plasma.core.warning=false;kf.windowsystem.warning=false;kf.kirigami.platform.warning=false",
         })
-        options.set_capability("timeouts", {'implicit': 30000})
-        cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', options=options)
+        cls.options.set_capability("timeouts", {'implicit': 30000})
+        cls.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', extensions=[SetValueCommand], options=cls.options)
 
     def tearDown(self) -> None:
         """
         Take screenshot when the current test fails
         """
         if not self._outcome.result.wasSuccessful():
-            self.driver.get_screenshot_as_file(f"failed_test_shot_plasmashell_#{self.id()}.png")
+            self.driver.get_screenshot_as_file(f"failed_test_shot_volume_#{self.id()}.png")
 
     @classmethod
     def tearDownClass(cls) -> None:
@@ -73,12 +100,39 @@ class DesktopTest(unittest.TestCase):
         Make sure to terminate the driver again, lest it dangles.
         """
         cls.driver.quit()
+        if cls.pulseaudio is not None:
+            cls.pulseaudio.terminate()
 
     def test_0_open(self) -> None:
         """
         Tests the widget can be opened
         """
         self.driver.find_element(AppiumBy.NAME, "No output or input devices found")
+
+    def test_1_sink_1_list_device(self) -> None:
+        """
+        Add a null sink
+        """
+        self.pulseaudio = subprocess.Popen(["pulseaudio"], stdout=sys.stderr, stderr=sys.stderr)
+        description: Final = "Virtual_Dummy_Output"
+        subprocess.check_call(["pactl", "load-module", "module-null-sink", "sink_name=DummyOutput", f"sink_properties=device.description={description}"])
+        self.driver.find_element(AppiumBy.NAME, description)
+
+        # Raise the maximum volume
+        slider_element: WebElement = self.driver.find_element(AppiumBy.NAME, f"Adjust volume for {description}")
+        self.driver.set_value(slider_element, str(int(0x10000 * 1.5)))  # PA_VOLUME_NORM * 1.5
+        wait = WebDriverWait(self.driver, 30)
+        wait.until(EC.presence_of_element_located((AppiumBy.NAME, "150%")))
+
+    def test_1_sink_2_bug472245_high_volume_on_startup(self) -> None:
+        """
+        Tests if the volume is set to the correct value on startup
+        @see https://bugs.kde.org/472245
+        """
+        self.driver.quit()
+        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4723', extensions=[SetValueCommand], options=self.options)
+        wait = WebDriverWait(self.driver, 30)
+        wait.until(EC.presence_of_element_located((AppiumBy.NAME, "150%")))
 
 
 if __name__ == '__main__':
