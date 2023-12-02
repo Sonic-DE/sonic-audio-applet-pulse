fa7975ecec9276429455777be5df37c1e007b566
Bharadwaj Raju <bharadwaj.raju777@protonmail.com>
2023-12-02 14:32:41 +0530

    Relicense some new files to LGPL instead of GPL for consistency

--- src/kded/audioshortcutsservice.h
+++ src/kded/audioshortcutsservice.h
@@ -1,6 +1,6 @@
 /*
     SPDX-FileCopyrightText: 2023 Bharadwaj Raju <bharadwaj.raju777@gmail.com>
-    SPDX-License-Identifier: GPL-3.0-or-later
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
 */
 
 #pragma once
