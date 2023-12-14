7bc46df96484cc290cf4c83c807fef1032fc3da9
Bharadwaj Raju <bharadwaj.raju777@protonmail.com>
2023-12-14 09:27:27 +0530

    Add SPDX

--- src/audioicon.cpp
+++ src/audioicon.cpp
@@ -1,3 +1,9 @@
+/*
+    SPDX-FileCopyrightText: 2023 Bharadwaj Raju <bharadwaj.raju777@protonmail.com>
+
+    SPDX-License-Identifier: LGPL-2.1-only OR LGPL-3.0-only OR LicenseRef-KDE-Accepted-LGPL
+*/
+
 #include "audioicon.h"
 
 #include <QMap>
